6784fca4e5f6c9b24fa962840d09655f5fbf1e98
Yongama <sobambela@gmail.com>
2019-06-18 13:43:33 +0200

    Pipline complete

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,14 +2,14 @@
 # Set permissions to storage and bootstrap cache
 sudo chmod -R 0775 /var/www/html
 sudo chown -R ec2-user:apache /var/www/html
-sudo chmod -R 0777 /var/www/html/storage
-sudo chmod -R 0777 /var/www/html/bootstrap/cache
+# sudo chmod -R 0777 /var/www/html/storage
+# sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
 cd /var/www/html
 #
 # Run composer
-sudo composer install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
-#
-# Run artisan commands
-php /var/www/html/artisan migrate
-php /var/www/html/artisan passport:keys
+# sudo composer install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
+# #
+# # Run artisan commands
+# php /var/www/html/artisan migrate
+# php /var/www/html/artisan passport:keys
