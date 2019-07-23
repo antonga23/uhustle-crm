4afd8bea00e3debc3f877374298f75255b40da08
Sone4 <ethasi@gmail.com>
2019-07-23 19:56:19 +0200

    Seed db each at each migrate, remove route from csrf

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -11,5 +11,5 @@ cd /var/www/html
 sudo composer install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
 # #
 # # Run artisan commands
-php /var/www/html/artisan migrate
+php /var/www/html/artisan migrate:refresh --seed
 
