caf320db6e1553181649fa7c7b1f24bf5175742c
Yongama <sobambela@gmail.com>
2019-08-12 19:24:58 +0200

    Created a development branch

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
 sudo chmod -R 0775 /var/www/html
-sudo chown -R ec2-user:apache /var/www/html
+sudo chown -R ubuntu:apache /var/www/html
 sudo chmod -R 0777 /var/www/html/storage
 sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
 cd /var/www/html
 #
 # Run composer
-# sudo composer install --no-ansi --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
+sudo composer install --no-ansi --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/html
 
 # sudo composer dumpautoload
 
 # #
 # # Run artisan commands
-# php /var/www/html/artisan migrate:fresh --seed
+php /var/www/html/artisan migrate --seed
 
