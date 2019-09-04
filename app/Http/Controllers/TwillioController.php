dd3e2a3a746e8fd566de30fadd9fdf4a404383de
Sone4 <ethasi@gmail.com>
2019-09-04 18:50:35 +0200

    Back to dialer v1

--- app/Http/Controllers/TwillioController.php
+++ app/Http/Controllers/TwillioController.php
@@ -133,43 +133,23 @@ class TwillioController extends Controller
 
         // Call
         $lead_id = $request->lead_id;
+        // $to_number = '+27619932376';
         $to_number = $request->phone_number;
-        $encodedSalesPhone = urlencode(str_replace(' ','',$twilio_number));
-        $host = config('app.url');
         
-        $account_sid = config('twillio.twillio_account_sid');
-        $auth_token = config('twillio.twillio_auth_token');
+        $response = new Twiml;
+        
+        if (isset($to_number) && strlen($to_number) > 0) {
+            
+            $dial = $response->dial(array('callerId' => $twilio_number));
 
-        $client = new Client($account_sid, $auth_token);
+            $dial->number($to_number);
 
-        try {
-            $client->calls->create(
-                $to_number, // The visitor's phone number
-                $twilio_number, // A Twilio number in your account
-                array(
-                    "url" => "$host/calls/outbound/$encodedSalesPhone"
-                )
-            );
-        } catch (Exception $e) {
-            // Failed calls will throw
-            return $e;
+        }else{
+            $response->say("Thanks for calling!");
+            
         }
-    
-        // return a JSON response
-        return array('message' => 'Call incoming!');
-    }
-
-    public function outBound ($salesPhone) {
-        // A message for Twilio's TTS engine to repeat
-        $sayMessage = 'Thanks for contacting our sales department. Our
-            next available representative will take your call.';
-    
-        $twiml = new Twiml();
-        $twiml->say($sayMessage, array('voice' => 'alice'));
-        $twiml->dial($salesPhone);
-    
-        return response($twiml, 200)
-                            ->header('Content-Type', 'text/xml'); 
+        
+        echo $response;
     }
 
     public function newToken(Request $request)
--- public/js/app.js
+++ public/js/app.js
@@ -78120,52 +78120,55 @@ var Device = __webpack_require__(/*! twilio-client */ "./node_modules/twilio-cli
             'lead_id': vm.lead_info.id,
             'contact_number': vm.lead_info.phone_number
           });
-          vm.call_status = 'Device Ready';
-          vm.$refs.callBtn.click(); // if(vm.role_id == vm.dialer_settings.applies_to_role && vm.dialer_settings.value == 'on'){ 
-          //     setTimeout( function(){
-          //         axios.get('/calls/token').then(function (response) {
-          //             console.log('Token',response.data.token);
-          //             // Setup Twilio.Device
-          //             Device.setup(response.data.token);
-          //             Device.on('ready',function (device) {
-          //                 vm.call_status = 'Device Ready';
-          //                 // vm.$refs.callBtn.click();
-          //             });
-          //             Device.on('error',function (error) {
-          //                 vm.call_status = 'Device Error: ' + error.message;
-          //             });
-          //             Device.on('connect',function (conn) {
-          //                 vm.call_status = 'Successfully established call';
-          //                 vm.call_back.call_sid = conn.parameters.CallSid;
-          //                 axios.post('/calls/create-call-record', {'lead_id' : vm.lead_info.id, 'call_sid' : conn.parameters.CallSid}).then(function (response) {
-          //                 }).catch(function (error) {                    
-          //                     console.log(error);
-          //                 });
-          //             });
-          //             Device.on('incoming', function (conn) {
-          //                 console.log('Incoming connection from ' + conn.parameters.From);
-          //                 var archEnemyPhoneNumber = '+12099517118';
-          //                 if (conn.parameters.From === archEnemyPhoneNumber) {
-          //                     conn.reject();
-          //                     console.log('It\'s your nemesis. Rejected call.');
-          //                 } else {
-          //                     // accept the incoming connection and start two-way audio
-          //                     conn.accept();
-          //                 }
-          //             });
-          //             Device.on('disconnect',function (conn) {
-          //                 vm.call_status = 'Call Disconnected';
-          //                 vm.$refs['final-call-step'].show();
-          //             });
-          //             vm.$Progress.finish();
-          //         }).catch(function (error) {                    
-          //             console.log(error);
-          //         });
-          //     }, 1000 );
-          // }else{
-          //     Fire.$emit('ShowGeneral');
-          //     vm.$Progress.finish();
-          // }
+
+          if (vm.role_id == vm.dialer_settings.applies_to_role && vm.dialer_settings.value == 'on') {
+            setTimeout(function () {
+              axios.get('/calls/token').then(function (response) {
+                console.log('Token', response.data.token); // Setup Twilio.Device
+
+                Device.setup(response.data.token);
+                Device.on('ready', function (device) {
+                  vm.call_status = 'Device Ready';
+                  vm.$refs.callBtn.click();
+                });
+                Device.on('error', function (error) {
+                  vm.call_status = 'Device Error: ' + error.message;
+                });
+                Device.on('connect', function (conn) {
+                  vm.call_status = 'Successfully established call';
+                  vm.call_back.call_sid = conn.parameters.CallSid;
+                  axios.post('/calls/create-call-record', {
+                    'lead_id': vm.lead_info.id,
+                    'call_sid': conn.parameters.CallSid
+                  }).then(function (response) {})["catch"](function (error) {
+                    console.log(error);
+                  });
+                });
+                Device.on('incoming', function (conn) {
+                  console.log('Incoming connection from ' + conn.parameters.From);
+                  var archEnemyPhoneNumber = '+12099517118';
+
+                  if (conn.parameters.From === archEnemyPhoneNumber) {
+                    conn.reject();
+                    console.log('It\'s your nemesis. Rejected call.');
+                  } else {
+                    // accept the incoming connection and start two-way audio
+                    conn.accept();
+                  }
+                });
+                Device.on('disconnect', function (conn) {
+                  vm.call_status = 'Call Disconnected';
+                  vm.$refs['final-call-step'].show();
+                });
+                vm.$Progress.finish();
+              })["catch"](function (error) {
+                console.log(error);
+              });
+            }, 1000);
+          } else {
+            Fire.$emit('ShowGeneral');
+            vm.$Progress.finish();
+          }
         } else {
           vm.$Progress.fail();
           vm.$swal('Failed', 'Opps, something went wrong while retrieving lead, please try again', 'warning');
@@ -78183,29 +78186,10 @@ var Device = __webpack_require__(/*! twilio-client */ "./node_modules/twilio-cli
       var form_data = {
         lead_id: vm.lead_info.id,
         // phone_number : vm.lead_info.contact_number,
-        phone_number: '+27738802485'
+        phone_number: '+27607152316'
       };
-      axios.post('/calls/call', form_data).then(function (response) {
-        console.log(response.data.message);
-      })["catch"](function (error) {
-        console.log(JSON.stringify(error));
-      });
+      Device.connect(form_data);
     },
-    // startCall() {
-    //     var vm = this;
-    //     this.idle = true;
-    //     this.show_edication_blocks = true;
-    //     this.general = false;
-    //     var audioCtx = new AudioContext();
-    //     audioCtx.resume();
-    //     Fire.$emit('InitiateCall');
-    //     var form_data = {
-    //         lead_id : vm.lead_info.id,
-    //         phone_number : vm.lead_info.contact_number,
-    //         phone_number : '+27682554070',
-    //     }
-    //     Device.connect(form_data);
-    // },
     endCall: function endCall() {
       var vm = this;
       Device.disconnectAll(function (conn) {
