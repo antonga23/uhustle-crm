45c4cba84704620a7b40c12a0a61769dc6d2085f
Sone4 <ethasi@gmail.com>
2019-09-19 20:45:19 +0200

    Removed composer dump autoload from After install

--- config/twillio.php
+++ config/twillio.php
@@ -1,27 +1,27 @@
 <?php 
 
-use DB;
-use Auth;
-use App\ApiIntegration;
+// use DB;
+// use Auth;
+// use App\ApiIntegration;
 
-$twillio = ApiIntegration::with('attributes')->where(['name' => 'Twillio'])->first();
+// $twillio = ApiIntegration::with('attributes')->where(['name' => 'Twillio'])->first();
 
-foreach ($twillio->attributes as $key => $value) {
-    switch($value->key){
-        case 'twilio_phone_number':
-                $twilio_phone_number = $value->value;
-            break;
-        case 'account_sid':
-                $account_sid = $value->value;
-            break;
-        case 'auth_token':
-                $auth_token = $value->value;
-            break;
-        case 'twiml_app_sid':
-                $twiml_app_sid = $value->value;
-            break;
-    }
-}
+// foreach ($twillio->attributes as $key => $value) {
+//     switch($value->key){
+//         case 'twilio_phone_number':
+//                 $twilio_phone_number = $value->value;
+//             break;
+//         case 'account_sid':
+//                 $account_sid = $value->value;
+//             break;
+//         case 'auth_token':
+//                 $auth_token = $value->value;
+//             break;
+//         case 'twiml_app_sid':
+//                 $twiml_app_sid = $value->value;
+//             break;
+//     }
+// }
 
 return [
     'twillio_number' => env('TWILLIO_NUMBER', $twillio_phone_number),
