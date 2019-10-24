735f80a8a518b1c29030d8bd27efcec34604c693
Beatrice <beatrice@winsta.io>
2019-10-24 02:27:27 -0700

    styling tweaks to tabs

--- resources/assets/js/components/Preferences/PreferencesIndex.vue
+++ resources/assets/js/components/Preferences/PreferencesIndex.vue
@@ -75,23 +75,28 @@
   }
 
   .btn-default{
-    padding: 6px 12px 6px 10px;
-    font-size: 9px;
-    border: transparent !important;
-    border-radius: 5px !important;
-    -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
-    -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+    background: #fff;
+    color: #999999;    
+    border: none!important;
+    padding: 11px 14px 10px;
+    font-size: 10px;
+    text-transform:uppercase;
+    border-radius: 50rem!important;
+    -webkit-box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
+    -moz-box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
   }
   .btn-default:hover{
-    background: #00344a;
-    color: #ffffff;    
-    border: transparent !important;
-    padding: 6px 12px 6px 10px;
-    font-size: 9px;
-    -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
-    -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+    border: none!important;
+    -webkit-box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
+    -moz-box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.05);
+  }
+  .btn-primary {
+    border-radius: 50rem!important;
+    text-transform:uppercase;
+    font-size: 10px;
+    padding: 11px 14px 10px;
   }
   .btn-block {
     display: block;
@@ -104,6 +109,25 @@
     padding-top: 0 !important;
     padding-bottom: 0;
   }
+  .divider-line { 
+    height: 1px; 
+    background-color: #003449; 
+  } 
+  .expand-toggle {
+    background: #fff;
+    box-shadow: none;
+    text-align: left;
+    color: #000;
+    border-radius: 0!important;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+  }
+  .custom-control-label {
+    color: #999999;
+    font-family: 'Rubik', sans-serif;
+    font-size:14px;
+  }
 </style>
 <template>
   <div id="preferences">
@@ -115,6 +139,7 @@
         </li> 
       </ul>
 		</nav>
+    
     <div class="row mx-0 top-nav">
       <ul class="top-menu">
         <li class="item">
@@ -125,6 +150,7 @@
             class="ml-0"
           >Roles</a>
         </li>
+
         <li class="item" style="display_none">
           <a
             href="#" 
@@ -132,6 +158,7 @@
             :class="{ 'active' : ( active_module_name ===  'dialer')? true : false }"
           >Dialer</a>
         </li>
+
         <li class="item">
           <a 
             href="#" 
@@ -139,6 +166,7 @@
             :class="{ 'active' : ( active_module_name ===  'api_integration')? true : false }"
           >API Integration</a>
         </li>
+
         <li class="item" v-for="(module, index) in modules" :key="index">
           <a 
             href="#" 
@@ -146,6 +174,7 @@
             :class="{ 'active' : ( active_module_name ===  module.tag)? true : false }"
           >{{ module.display_name }}</a>
         </li>
+
         <li class="item">
           <a 
             role="button" 
@@ -158,156 +187,170 @@
       </ul>
     </div>
 
-    <div>
-      <div class="row mx-0 stats scroll-hidden">
-        <div class="col-lg-12 px-0">
-          <vcl-table v-if="show_page_loader" > </vcl-table>
-          <div class="col-lg-12 px-0 user-roles" v-if="!show_page_loader && active_module_name == 'roles'">
-            <b-card no-body>
-              <b-tabs card>
-                <b-tab 
-                  :title="role.display_name" 
-                  @click="editRole(role)" 
-                  v-for="(role,index) in roles" 
-                  :key="index" :active="(index == 0)? true : false"
-                >
-                  <div class="row mx-0" v-if="role_edit">
-                    <edit-role :role="role" />
-                  </div>
-                  <div class="row mx-0">
-                    <b-button class="btn btn-success" @click="updateRole(role)">Update {{ role.display_name }}</b-button>
-                    <b-button class="btn btn-danger" @click="deleteRole(role)">Delete {{ role.display_name }}</b-button>
-                  </div>
-                  <div class="row mx-0" v-if="role_add">
-                    <add-role/>
-                  </div>
-                  <div class="row mx-0">
-                    <hr>
-                  </div>
-                  <transition name="fade">
-                    <div class="row mx-0" v-if="!role_add" >
-                      <div role="tablist" class="col-lg-12">
-                        <h5>Permissions</h5>
-                        <div>
-                          <div>
-                            <b-card no-body class="mb-1">
-                              <b-card-header header-tag="header" class="p-1" role="tab">
-                                <b-button 
-                                  block 
-                                  href="#" 
-                                  v-b-toggle="'accordion-0'" 
-                                  :aria-controls="'accordion-0'" 
-                                  variant="info"
-                                >Dialer</b-button>
-                              </b-card-header>
-                              <b-collapse :id="'accordion-0'" visible accordion="my-accordion" role="tabpanel">
-                                <b-card-body>
-                                  <div v-for="(permission,k) in dialer_permissions" :key="k">
-                                    <div v-if="permission.role_id == role.id">
-                                      <b-form-group  class="permisions">
-                                        <a-row >
-                                          <a-col :span="8">
-                                            <b-form-checkbox 
-                                              inline value="1" 
-                                              unchecked-value="0" 
-                                              v-model="permission.disabled" 
-                                              @change="applyDialerPermissions()"
-                                            >{{ (permission.disabled == 1)? 'Enabled' : 'Disabled' }}</b-form-checkbox>
-                                          </a-col>
-                                          <a-col :span="8">    
-                                            <b-form-checkbox 
-                                              inline 
-                                              value="1" 
-                                              unchecked-value="0" 
-                                              v-model="permission.whisper" 
-                                              @change="applyDialerPermissions()"
-                                            >Whisper </b-form-checkbox>
-                                          </a-col>
-                                          <a-col :span="8">    
-                                            <b-form-checkbox 
-                                              inline value="1" 
-                                              unchecked-value="0" 
-                                              v-model="permission.barge" 
-                                              @change="applyDialerPermissions()"
-                                            >Barge</b-form-checkbox>
-                                          </a-col>
-                                        </a-row>
-                                      </b-form-group>
-                                    </div> 
-                                  </div>
-                                </b-card-body>
-                              </b-collapse>
-                            </b-card>
-                          </div>
-                          <div v-for="(a_module, i) in modules" :key="i">
-                            <b-card no-body class="mb-1">
-                              <b-card-header header-tag="header" class="p-1" role="tab">
-                                <b-button 
-                                  block href="#" 
-                                  v-b-toggle="'accordion-' + (i+1)"  
-                                  :aria-controls="'accordion-' + (i+1)" 
-                                  variant="info"
-                                >{{ a_module.display_name }}</b-button>
-                              </b-card-header>
-                              <b-collapse :id="'accordion-' + (i+1)" accordion="my-accordion" role="tabpanel">
-                                <div v-for="(permission, k) in permissions" :key="k" >
-                                  <b-card-body v-if="permission.module_id == a_module.id && permission.role_id == role.id">
-                                    <a-row >
-                                      <a-col :span="8">
-                                      <b-form-checkbox 
-                                        inline 
-                                        value="1" 
-                                        unchecked-value="0" 
-                                        v-model="permission.read" 
-                                        @change="updatePermmissions()"
-                                      >Read </b-form-checkbox>
-                                      </a-col>
-                                      <a-col :span="8">
-                                        <b-form-checkbox 
-                                          inline 
-                                          value="1" 
-                                          unchecked-value="0" 
-                                          v-model="permission.write" 
-                                          @change="updatePermmissions()"
-                                        >Edit </b-form-checkbox>
-                                      </a-col>
-                                      <a-col :span="8">
-                                        <b-form-checkbox 
-                                          inline 
-                                          value="1" 
-                                          unchecked-value="0" 
-                                          v-model="permission.delete" 
-                                          @change="updatePermmissions()"
-                                        >Delete </b-form-checkbox>
-                                      </a-col>
-                                    </a-row>
-                                  </b-card-body>
-                                </div>
-                              </b-collapse>
-                            </b-card>
-                          </div>
+    <div class="row mx-0 stats scroll-hidden">
+      <div class="col-lg-12 px-0">
+        <vcl-table v-if="show_page_loader" > </vcl-table>
+        <div class="user-roles" v-if="!show_page_loader && active_module_name == 'roles'">
+          <b-card no-body>
+            <b-tabs card>
+              <b-tab 
+                :title="role.display_name" 
+                @click="editRole(role)" 
+                v-for="(role,index) in roles" 
+                :key="index" :active="(index == 0)? true : false">
+                <div class="row mx-0" v-if="role_edit">
+                  <edit-role :role="role" />
+                </div>
+
+                <div class="row mx-0">
+                  <b-button class="btn btn-danger" @click="deleteRole(role)">Delete {{ role.display_name }}</b-button>
+                  <b-button class="btn btn-default" @click="updateRole(role)">Cancel</b-button>
+                  <b-button class="btn btn-primary font-weight-bold" @click="updateRole(role)">Update</b-button>
+                </div>
+
+                <div class="row mx-0" v-if="role_add">
+                  <add-role/>
+                </div>
+                
+                <transition name="fade">
+                  <div class="row mx-0" v-if="!role_add" >
+                    <div role="tablist" class="col-lg-12">
+                      <div class="row mx-0 align-items-center">
+                        <div class="col-auto pl-0">
+                          <h5 class="mb-0">Permissions</h5>
+                        </div>
+
+                        <div class="col px-0">
+                          <div class="divider-line"></div>
                         </div>
                       </div>
+
+                      <b-button 
+                        block 
+                        href="#" 
+                        v-b-toggle="'accordion-0'" 
+                        :aria-controls="'accordion-0'"
+                        class="expand-toggle"
+                      >
+                        Dialer
+                        <!-- <img src="images/icons/up.svg" width="11"> -->
+                        <img src="images/icons/down.svg" width="11">
+                      </b-button>
+
+                      <b-collapse :id="'accordion-0'" visible accordion="my-accordion" role="tabpanel">
+                        <b-card>
+                          <div v-for="(permission,k) in dialer_permissions" :key="k">
+                            <div v-if="permission.role_id == role.id">
+                              <b-form-group  class="permisions">
+                                <a-row >
+                                  <a-col :span="8">
+                                    <b-form-checkbox 
+                                      inline value="1" 
+                                      unchecked-value="0" 
+                                      v-model="permission.disabled" 
+                                      @change="applyDialerPermissions()"
+                                    >{{ (permission.disabled == 1)? 'Enabled' : 'Disabled' }}</b-form-checkbox>
+                                  </a-col>
+
+                                  <a-col :span="8">    
+                                    <b-form-checkbox 
+                                      inline 
+                                      value="1" 
+                                      unchecked-value="0" 
+                                      v-model="permission.whisper" 
+                                      @change="applyDialerPermissions()"
+                                    >Whisper </b-form-checkbox>
+                                  </a-col>
+
+                                  <a-col :span="8">    
+                                    <b-form-checkbox 
+                                      inline value="1" 
+                                      unchecked-value="0" 
+                                      v-model="permission.barge" 
+                                      @change="applyDialerPermissions()"
+                                    >Barge</b-form-checkbox>
+                                  </a-col>
+                                </a-row>
+                              </b-form-group>
+                            </div> 
+                          </div>
+                        </b-card>
+                      </b-collapse>
+
+                      <div v-for="(a_module, i) in modules" :key="i">
+                        <b-button 
+                          block href="#" 
+                          v-b-toggle="'accordion-' + (i+1)"  
+                          :aria-controls="'accordion-' + (i+1)" 
+                          class="border-radius-0 expand-toggle"
+                        >
+                          {{ a_module.display_name }}
+                          <!-- <img src="images/icons/up.svg" width="11"> -->
+                          <img src="images/icons/down.svg" width="11">
+                        </b-button>
+                          
+                        <b-collapse :id="'accordion-' + (i+1)" accordion="my-accordion" role="tabpanel">
+                          <div v-for="(permission, k) in permissions" :key="k" >
+                            <b-card v-if="permission.module_id == a_module.id && permission.role_id == role.id">
+                              <a-row>
+                                <a-col :span="8">
+                                  <b-form-checkbox 
+                                    inline 
+                                    value="1" 
+                                    unchecked-value="0" 
+                                    v-model="permission.read" 
+                                    @change="updatePermmissions()"
+                                  >Read</b-form-checkbox>
+                                </a-col>
+
+                                <a-col :span="8">
+                                  <b-form-checkbox 
+                                    inline 
+                                    value="1" 
+                                    unchecked-value="0" 
+                                    v-model="permission.write" 
+                                    @change="updatePermmissions()"
+                                  >Edit </b-form-checkbox>
+                                </a-col>
+
+                                <a-col :span="8">
+                                  <b-form-checkbox 
+                                    inline 
+                                    value="1" 
+                                    unchecked-value="0" 
+                                    v-model="permission.delete" 
+                                    @change="updatePermmissions()"
+                                  >Delete </b-form-checkbox>
+                                </a-col>
+                              </a-row>
+                            </b-card>
+                          </div>
+                        </b-collapse>
+                      </div>
                     </div>
-                  </transition>
-                </b-tab>
-                <b-tab :title="'+'" @click="addRole">
-                  <div class="row mx-0" v-if="role_add">
-                    <add-role/>
                   </div>
-                </b-tab>
-              </b-tabs>
-            </b-card>
-          </div>
-          <div class="col-lg-12  user-roles" v-if="!show_page_loader && active_module_action == 'api_integration'">
-            <api-integration :apis="apis"/>
-          </div>
-          <div class="col-lg-12  user-roles" v-if="!show_page_loader && active_module_action == 'add_module'">
-            <add-module/>
-          </div>
-          <div class="col-lg-12  user-roles" v-if="!show_page_loader && active_module_action == 'edit_module'">
-            <edit-module :in_module="editing_module"/>
-          </div>
+                </transition>
+              </b-tab>
+
+              <b-tab :title="'+'" @click="addRole">
+                <div class="row mx-0" v-if="role_add">
+                  <add-role/>
+                </div>
+              </b-tab>
+            </b-tabs>
+          </b-card>
+        </div>
+
+        <div class="user-roles" v-if="!show_page_loader && active_module_action == 'api_integration'">
+          <api-integration :apis="apis"/>
+        </div>
+
+        <div class="user-roles" v-if="!show_page_loader && active_module_action == 'add_module'">
+          <add-module/>
+        </div>
+
+        <div class="user-roles" v-if="!show_page_loader && active_module_action == 'edit_module'">
+          <edit-module :in_module="editing_module"/>
         </div>
       </div>
     </div>
