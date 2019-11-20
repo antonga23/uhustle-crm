813ac7aca3894956151a86fcbd3cc18a13a79c43
Beatrice <beatrice@winsta.io>
2019-11-20 01:13:49 -0800

    changed uhustle logo to siarem logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,16 +27,16 @@
               <div class="row justify-content-center mx-auto">
                 <div class="col-lg-12 mb-3 p-0 center">
                   <a class="navbar-brand m-0 p-0" href="{{ url('/') }}">
-                    <img id="top-landing-logo" src="/images/SVG_Images/Logo.svg" class=" lp-logo m-0">
+                    <img id="top-landing-logo" src="/images/SVG_Images/siarem-logo.svg" class="lp-logo m-0">  
                   </a>
                 </div>
               </div>
               
-              <div class="row justify-content-center">
+              <!-- <div class="row justify-content-center">
                 <div class="col-lg-12 mb-0 p-0 center">
                   <p class="logo-title mb-0">UHUSTLE</p>
                 </div>
-              </div>
+              </div> -->
 
               <div class="container register px-0">
                 <div class="row justify-content-center">
