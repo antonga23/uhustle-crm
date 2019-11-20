d9e2830f43a9d8d65c40c242215323a6a1f889a1
Beatrice <beatrice@winsta.io>
2019-11-20 08:49:27 -0800

    rebuilt login and signup pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,76 @@
 @extends('layouts.auth') @section('content')
 <div id="app">
   <div class="main-wrapper min-vh-100" id="auth">
-    <div class="row justify-content-center align-items-center min-vh-100 mx-0 ">
-      <div class="col-lg-4 min-vh-100 outer-login-container left-side">
+    <div class="row justify-content-center align-items-center min-vh-100 mx-0">
+      <div class="col-6 px-0">
+        <div class="login">
+          <h1 class="text-center">Login</h1>
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="form-group mt-4">
+              <input 
+                id="email" 
+                placeholder="{{ __('Email') }}" 
+                type="email" 
+                class="form-control @error('email') is-invalid @enderror omni-shadow rounded-pill" 
+                name="email" 
+                value="{{ old('email') }}" 
+                required 
+                autocomplete="email" 
+                autofocus
+              > @error('email')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span> @enderror
+            </div>
+
+            <div class="form-group mb-0">
+              <input 
+                id="password" 
+                placeholder="{{ __('Password') }}" 
+                type="password" 
+                class="form-control @error('password') is-invalid @enderror omni-shadow rounded-pill" 
+                name="password" 
+                required 
+                autocomplete="current-password"
+              >@error('password')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span> @enderror
+            </div>
+
+            <div class="form-group row mx-0 mb-0 justify-content-between route-btns">
+              <div class="col-auto">
+                <a href="/register" class="btn center m-0 px-0 auth-btns font-weight-bold">Sign up</a>
+              </div>
+
+              <div class="col-auto">
+                @if (Route::has('password.request'))
+                <a class="btn btn-link m-0 px-0 text-capitalize pwd-link font-weight-bold" href="{{ route('password.request') }}">
+                  {{ __('forgot password?') }}
+                </a> @endif 
+              </div>
+            </div>
+
+            <div class="form-group row mx-0 mb-0 justify-content-center">
+              <div class="col-auto">
+                <button type="submit" class="btn btn-primary submit-btn mx-0 mb-0 font-weight-bold text-uppercase border-0">
+                  {{ __('Log in') }}
+                </button>
+              </div>
+            </div>
+          </form>
+          <hr class="copyright-divider">
+          <p class="copyright">@ 2019 SIAREM. All rights reserved</p>
+        </div>
+      </div>
+
+      <div class="col-6 text-right">
+        <img src="/images/SVG_Images/siarem-logo.svg" alt="Siarem logo" width="80.5%">
+      </div>
+
+      <!-- <div class="col-lg-4 min-vh-100 outer-login-container left-side">
         <div class="row login justify-content-center inner-login-container mx-auto">
           <div class="col-lg-12 mb-0 p-0">
             <div class="row mx-0">
@@ -20,7 +88,6 @@
           </div>
         </div>
       </div>
-
       <div class="col-lg-8 px-5 right-side">
         <main>
           <div class="row align-items-center justify-content-center">
@@ -32,12 +99,6 @@
                   </a>
                 </div>
               </div>
-
-              <!-- <div class="row justify-content-center">
-                <div class="col-lg-12 mb-0 p-0 center">
-                  <img id="top-landing-logo" src="/images/SVG_Images/siarem-logo-text.svg" class=" lp-logo m-0">
-                </div>
-              </div> -->
 
               <div class="container login px-0">
                 <div class="row justify-content-center">
@@ -76,14 +137,14 @@
                       </div>
                     </form>
                     <hr class="my-4">
-                    <p class="copyright">@ 2019 SIAREM. All rights reserve</p>
+                    <p class="copyright">@ 2019 UHUSTLE. All rights reserve</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </main>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,68 @@
 @extends('layouts.auth') @section('content')
 <div id="app">
   <div class="main-wrapper min-vh-100" id="auth">
-    <div class="row justify-content-center align-items-center min-vh-100 mx-0 ">
-      <div class="col-lg-4 min-vh-100 outer-login-container left-side">
+    <div class="row justify-content-center align-items-center min-vh-100 mx-0">
+      <div class="col-6 px-0">
+        <div class="login">
+          <h1 class="text-center">Sign Up</h1>
+          <form role="form" method="POST" action="{{ route('register') }}">
+            <div class="form-group">
+              <input id="name" type="text" class="form-control @error('name') is-invalid @enderror rounded-pill omni-shadow" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name"> @error('name')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span> @enderror
+            </div>
+
+            <div class="form-group pr-0">
+              <input maxlength="100" type="text" required="required" class="form-control rounded-pill omni-shadow" placeholder="Surname" />
+            </div>
+
+            <div class="form-group">
+              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror rounded-pill omni-shadow" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email"> @error('email')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span> @enderror
+            </div>
+
+            <div class="form-group">
+              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror rounded-pill omni-shadow" name="password" required autocomplete="new-password" placeholder="Password"> @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span> @enderror
+            </div>
+
+            <div class="form-group row mx-0 mb-0 justify-content-between route-btns">
+              <div class="col-auto custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" name="" id="check5">
+                <label class="terms-text custom-control-label" for="check5">I agree to the 
+                  <a class="font-weight-bold terms" href=""> terms and conditions</a>
+                </label>
+              </div>
+
+              <div class="col-auto">
+                <a href="/register" class="btn center m-0 p-0 auth-btns font-weight-bold">Login</a>
+              </div>
+            </div>
+
+            <div class="row justify-content-center mx-0">
+              <div class="col-auto">
+                <button type="submit" class="btn btn-primary submit-btn mx-0 mb-0 font-weight-bold text-uppercase border-0">
+                  {{ __('Get Started') }}
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <hr class="copyright-divider">
+          <p class="copyright">@ 2019 SIAREM. All rights reserved</p>
+        </div>
+      </div>
+
+      <div class="col-6 text-right">
+        <img src="/images/SVG_Images/siarem-logo.svg" alt="Siarem logo" width="80.5%">
+      </div>
+
+      <!-- <div class="col-lg-4 min-vh-100 outer-login-container left-side">
         <div class="row login justify-content-center inner-login-container mx-auto">
           <div class="col-lg-12 mb-0 p-0">
             <div class="row mx-0">
@@ -31,12 +91,6 @@
                   </a>
                 </div>
               </div>
-              
-              <!-- <div class="row justify-content-center">
-                <div class="col-lg-12 mb-0 p-0 center">
-                  <p class="logo-title mb-0">siarem</p>
-                </div>
-              </div> -->
 
               <div class="container register px-0">
                 <div class="row justify-content-center">
@@ -90,7 +144,7 @@
             </div>
           </div>
         </main>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
