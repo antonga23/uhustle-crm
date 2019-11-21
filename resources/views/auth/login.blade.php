f5d86f46b244b00cc9e687f3cb5ac9fe05d74452
Beatrice <beatrice@winsta.io>
2019-11-21 01:20:42 -0800

    further styled new login and signup pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
                 autofocus
               > @error('email')
               <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
+                {{ $message }}
               </span> @enderror
             </div>
 
@@ -36,7 +36,7 @@
                 autocomplete="current-password"
               >@error('password')
               <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
+                {{ $message }}
               </span> @enderror
             </div>
 
@@ -69,82 +69,6 @@
       <div class="col-6 text-right">
         <img src="/images/SVG_Images/siarem-logo.svg" alt="Siarem logo" width="80.5%">
       </div>
-
-      <!-- <div class="col-lg-4 min-vh-100 outer-login-container left-side">
-        <div class="row login justify-content-center inner-login-container mx-auto">
-          <div class="col-lg-12 mb-0 p-0">
-            <div class="row mx-0">
-              <div class="col-md-12">
-                <a href="/login" class="btn center pt-4 w-100 m-0 activeBtn">Login</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-12 mb-4 p-0">
-            <div class="row mx-0">
-              <div class="col-md-12">
-                <a href="/register" class="btn center pt-4 w-100 m-0 inactiveBtn">Sign Up</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-8 px-5 right-side">
-        <main>
-          <div class="row align-items-center justify-content-center">
-            <div class="col-10">
-              <div class="row justify-content-center mx-auto">
-                <div class="col-lg-12 mb-4 p-0 center">
-                  <a class="navbar-brand m-0 p-0" href="{{ url('/') }}">
-                    <img id="top-landing-logo" src="/images/SVG_Images/siarem-logo.svg" class="lp-logo m-0">
-                  </a>
-                </div>
-              </div>
-
-              <div class="container login px-0">
-                <div class="row justify-content-center">
-                  <div class="col-lg-12">
-                    <form method="POST" action="{{ route('login') }}">
-                      @csrf
-
-                      <div class="form-group row mt-4">
-                        <div class="col-md-12">
-                          <input id="email" placeholder="{{ __('Email') }}" type="email" class="form-control @error('email') is-invalid @enderror omni-shadow rounded-pill" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> @error('email')
-                          <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                          </span> @enderror
-                        </div>
-                      </div>
-
-                      <div class="form-group row mb-0">
-                        <div class="col-md-12">
-                          <input id="password" placeholder="{{ __('Password') }}" type="password" class="form-control @error('password') is-invalid @enderror omni-shadow rounded-pill" name="password" required autocomplete="current-password"> @error('password')
-                          <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                          </span> @enderror
-                        </div>
-                      </div>
-
-                      @if (Route::has('password.request'))
-                      <a class="btn btn-link w-100 mx-0 mt-0 mb-3 pwd-link" href="{{ route('password.request') }}">
-                        {{ __('forgot password?') }}
-                      </a> @endif 
-                      <div class="form-group row mb-0">
-                        <div class="col-md-12">
-                          <button type="submit" class="btn btn-primary submit-btn w-100 m-0">
-                            {{ __('Login') }}
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                    <hr class="my-4">
-                    <p class="copyright">@ 2019 UHUSTLE. All rights reserve</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div> -->
     </div>
   </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
             <div class="form-group">
               <input id="name" type="text" class="form-control @error('name') is-invalid @enderror rounded-pill omni-shadow" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name"> @error('name')
               <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
+                {{ $message }}
               </span> @enderror
             </div>
 
@@ -20,14 +20,14 @@
             <div class="form-group">
               <input id="email" type="email" class="form-control @error('email') is-invalid @enderror rounded-pill omni-shadow" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email"> @error('email')
               <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
+                {{ $message }}
               </span> @enderror
             </div>
 
             <div class="form-group">
               <input id="password" type="password" class="form-control @error('password') is-invalid @enderror rounded-pill omni-shadow" name="password" required autocomplete="new-password" placeholder="Password"> @error('password')
               <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
+                {{ $message }}
               </span> @enderror
             </div>
 
@@ -61,90 +61,6 @@
       <div class="col-6 text-right">
         <img src="/images/SVG_Images/siarem-logo.svg" alt="Siarem logo" width="80.5%">
       </div>
-
-      <!-- <div class="col-lg-4 min-vh-100 outer-login-container left-side">
-        <div class="row login justify-content-center inner-login-container mx-auto">
-          <div class="col-lg-12 mb-0 p-0">
-            <div class="row mx-0">
-              <div class="col-md-12">
-                <a href="/login" class="btn center pt-4 w-100 m-0 inactiveBtn">Login</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-12 mb-4 p-0">
-            <div class="row mx-0">
-              <div class="col-md-12">
-                <a href="/register" class="btn center pt-4 w-100 m-0 activeBtn">Sign Up</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-8 px-5 right-side">
-        <main>
-          <div class="row align-items-center justify-content-center">
-            <div class="col-10">
-              <div class="row justify-content-center mx-auto">
-                <div class="col-lg-12 mb-3 p-0 center">
-                  <a class="navbar-brand m-0 p-0" href="{{ url('/') }}">
-                    <img id="top-landing-logo" src="/images/SVG_Images/siarem-logo.svg" class="lp-logo m-0">  
-                  </a>
-                </div>
-              </div>
-
-              <div class="container register px-0">
-                <div class="row justify-content-center">
-                  <div class="col-lg-12">
-                    <div class="container px-0">
-                      <form role="form" method="POST" action="{{ route('register') }}">
-                        <div class="form-group">
-                          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror rounded-pill omni-shadow" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name"> @error('name')
-                          <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                          </span> @enderror
-                        </div>
-
-                        <div class="form-group pr-0">
-                          <input maxlength="100" type="text" required="required" class="form-control rounded-pill omni-shadow" placeholder="Surname" />
-                        </div>
-
-                        <div class="form-group">
-                          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror rounded-pill omni-shadow" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email"> @error('email')
-                          <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                          </span> @enderror
-                        </div>
-
-                        <div class="form-group">
-                          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror rounded-pill omni-shadow" name="password" required autocomplete="new-password" placeholder="Password"> @error('password')
-                          <span class="invalid-feedback" role="alert">
-                              <strong>{{ $message }}</strong>
-                          </span> @enderror
-                        </div>
-
-                        <div class="pb-4 custom-control custom-checkbox">
-                          <input type="checkbox" class="custom-control-input" name="" id="check5">
-                          <label class="terms-text custom-control-label" for="check5">I agree to the <a class="terms" href=""> terms and conditions</a></label>
-                        </div>
-
-                        <div class="row justify-content-center mx-0">
-                          <div class="col-md-12 px-0">
-                            <button type="submit" class="btn btn-primary submit-btn w-100 m-0">
-                              {{ __('Get Started') }}
-                            </button>
-                          </div>
-                        </div>
-                        <hr class="copyright-divider">
-                        <p class="copyright">@ 2019 SIAREM. All rights reserve</p>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div> -->
     </div>
   </div>
 </div>
