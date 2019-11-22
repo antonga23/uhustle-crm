1dd425777ce4fe86c0a0fc9ae8b1fc4a88cfa2bf
Yongama <sobambela@gmail.com>
2019-11-22 11:19:25 +0200

    sorces on workstation

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -49,6 +49,7 @@ class PagesController extends Controller
               ->with(['active'=> 'workstation'])
               ->with(['item_id'=> $item_id])
               ->with(['custom_fields'=> $custom_fields])
+              ->with(['sources'=> LeadSource::get()])
               ->with(['lead_id'=> ''])
               ->with(['auto_dialer_settings' => $auto_dialer_settings]);
 
