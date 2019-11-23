8eba4da50a8ae2ecc9b6dc16f8e6350de638a6d7
Yongama <sobambela@gmail.com>
2019-11-23 19:48:55 +0200

    Order creating missing the product row to add quantiity

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -20,6 +20,6 @@ sudo composer install --no-ansi --no-suggest --no-interaction --no-progress --pr
 # # Run artisan commands
 
 php /var/www/html/artisan migrate
-# php /var/www/html/artisan db:seed
+php /var/www/html/artisan db:seed
 php /var/www/html/artisan config:clear
 
