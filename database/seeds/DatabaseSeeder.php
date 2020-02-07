7ae6f6a3debc43a16d8d269bda546b1e83dfd7dc
alatha <alatha@uhustle.com>
2020-02-07 18:25:09 +0200

    minimum working configuration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,19 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RolesTablesSeeder::class);
-        // $this->call(SourcesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(LeadsTableSeeder::class);
-        // $this->call(SystemSettingsSeeder::class);
-        // $this->call(ModuleTableSeeder::class);
-        // $this->call(PermissionsTableSeeder::class);
-        // $this->call(ApiIntegrationTableSeeder::class);
-        // $this->call(CompanyTypeSeeder::class);
-        // $this->call(ProductCategoryTableSeeder::class);
-        // $this->call(OrderTypeTableSeeder::class);
-        // $this->call(OrderClassTableSeeder::class);
-        // $this->call(ProvinceTableSeeder::class);
-        // $this->call(TaxTableSeeder::class);
+         $this->call(RolesTablesSeeder::class);
+         $this->call(SourcesTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+         $this->call(LeadsTableSeeder::class);
+         $this->call(SystemSettingsSeeder::class);
+         $this->call(ModuleTableSeeder::class);
+         $this->call(PermissionsTableSeeder::class);
+         $this->call(ApiIntegrationTableSeeder::class);
+         $this->call(CompanyTypeSeeder::class);
+         $this->call(ProductCategoryTableSeeder::class);
+         $this->call(OrderTypeTableSeeder::class);
+         $this->call(OrderClassTableSeeder::class);
+         $this->call(ProvinceTableSeeder::class);
+         $this->call(TaxTableSeeder::class);
     }
 }
